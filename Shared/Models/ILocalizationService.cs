af0f3883513ae543a3c349d3de8be29cf8756fcb
Vic Rubba <crazedVic@users.noreply.github.com>
2025-02-13 00:51:01 -0700

    cutlureselector now geting options from LocalizationService

--- Shared/Models/ILocalizationService.cs
+++ Shared/Models/ILocalizationService.cs
@@ -3,6 +3,7 @@ namespace BlazorHelloWorld.Shared.Models;
 public interface ILocalizationService
 {
     string CurrentLanguage { get; }
+    List<(string Code, string Name)> AvailableLanguages { get; }
     event Action? LanguageChanged;
     Task SetLanguage(string language);
     void AddTranslation(string key, string language, LocalizedProduct translation);
